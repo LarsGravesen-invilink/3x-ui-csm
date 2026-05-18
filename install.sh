fac7af5b680304d6fdd9d18e7cab6079ccc630f6
LarsGravesen <igor@telyuk.ru>
2026-05-18 04:06:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-set -e
+    set -e
 
-REPO="https://raw.githubusercontent.com/LarsGravesen-invilink/3x-ui-csm/main"
-BASE_DIR="/opt/3xcsm"
-BIN="/usr/local/bin/3xsub"
-
-mkdir -p "$BASE_DIR"
-
-curl -fsSL "$REPO/3xcsm.sh" -o "$BASE_DIR/3xcsm.sh"
-chmod +x "$BASE_DIR/3xcsm.sh"
-
-cat > "$BIN" <<'EOF'
-#!/bin/bash
-exec bash /opt/3xcsm/3xcsm.sh --menu "$@"
-EOF
+    curl -fsSL https://raw.githubusercontent.com/LarsGravesen-invilink/3x-ui-csm/main/install.sh -o /tmp/3xcsm.sh
+    chmod +x /tmp/3xcsm.sh
+    bash /tmp/3xcsm.sh
 
 chmod +x "$BIN"
 
-# запуск сразу после установки
 bash /opt/3xcsm/3xcsm.sh --menu
