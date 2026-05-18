d8405772d9b605ff1123b369f7096aedfc54576f
LarsGravesen <igor@telyuk.ru>
2026-05-18 04:11:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-    set -e
+set -e
 
-    curl -fsSL https://raw.githubusercontent.com/LarsGravesen-invilink/3x-ui-csm/main/install.sh -o /tmp/3xcsm.sh
-    chmod +x /tmp/3xcsm.sh
-    bash /tmp/3xcsm.sh
-
-chmod +x "$BIN"
-
-bash /opt/3xcsm/3xcsm.sh --menu
+curl -fsSL https://raw.githubusercontent.com/LarsGravesen-invilink/3x-ui-csm/main/install.sh -o /tmp/3xcsm.sh
+chmod +x /tmp/3xcsm.sh
+bash /tmp/3xcsm.sh
